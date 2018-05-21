629f8f18b4c91aac6605dbc0219311bc6699fdb5
Douglas <douglas.j.ashton@gmail.com>
2018-05-22 00:06:10 +0100

    reset deploy.sh

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -47,10 +47,10 @@ echo "*keys.tar" >> .gitignore
 echo "scripts/" >> .gitignore
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-#if git diff --quiet; then
-#    echo "No changes to the output on this push; exiting."
-#    exit 0
-#fi
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
