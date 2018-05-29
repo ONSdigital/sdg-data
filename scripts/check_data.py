05b3d392bf1dae087c550a9cbd0696517676d75e
Douglas Ashton <douglas.j.ashton@gmail.com>
2018-05-29 17:35:39 +0200

    [WIP] try downloading yamlmd at build time (#9)
    Download packages at build time

---
+++ scripts/check_data.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-10-04
+
+@author: dougashton
+"""
+
+# %% setup
+
+import glob
+import pandas as pd
+import numpy as np
+import sdg
+
+def main():
+    """Run csv checks on all indicator csvs in the data directory"""
+    status = True
+    
+    status = status & sdg.check_all_csv()
+    status = status & sdg.check_all_meta()
+
+    return status
+
+if __name__ == '__main__':
+    status = main()
+    if(not status):
+        raise RuntimeError("Failed checks")
+    else:
+        print("Success")
