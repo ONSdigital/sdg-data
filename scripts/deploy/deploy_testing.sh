d19789a34437182e9ace83aa5c826dc506260bdc
Douglas Ashton <douglas.j.ashton@gmail.com>
2019-03-04 15:34:55 +0000

    deploy with rsync

--- scripts/deploy/deploy_testing.sh
+++ scripts/deploy/deploy_testing.sh
@@ -23,8 +23,8 @@ rm scripts/deploy/keys.tar
 chmod 600 ./scripts/deploy/deploy_key_test
 eval `ssh-agent -s`
 ssh-add scripts/deploy/deploy_key_test
-rm scripts/deploy/deploy_key*
+#rm scripts/deploy/deploy_key*
 
 # Push the files over, removing anything existing already.
 ssh -oStrictHostKeyChecking=no travis@$TEST_SERVER "rm -rf ~/www/data/$BASEURL || true"
-scp -oStrictHostKeyChecking=no -rC _site travis@$TEST_SERVER:~/www/data/$BASEURL
+rsync -avz -e "ssh -i scripts/deploy/deploy_key_test -oStrictHostKeyChecking=no" --link-dest="../sdg-indicators" _site/ travis@$TEST_SERVER:~/www/data/$BASEURL
