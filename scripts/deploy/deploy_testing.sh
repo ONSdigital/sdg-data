e311758a8d7e23ff66c1536980c2455b282e1a1a
Douglas Ashton <douglas.j.ashton@gmail.com>
2019-03-04 16:16:37 +0000

    don't add key

--- scripts/deploy/deploy_testing.sh
+++ scripts/deploy/deploy_testing.sh
@@ -21,10 +21,8 @@ tar xvf scripts/deploy/keys.tar -C scripts/deploy/
 rm scripts/deploy/keys.tar
 
 chmod 600 ./scripts/deploy/deploy_key_test
-eval `ssh-agent -s`
-ssh-add scripts/deploy/deploy_key_test
 
 # Push the files over, removing anything existing already.
 ssh -oStrictHostKeyChecking=no travis@$TEST_SERVER "rm -rf ~/www/data/$BASEURL || true"
-rsync -rvz -e "ssh -i scripts/deploy/deploy_key_test -oStrictHostKeyChecking=no" --checksum --link-dest="../develop" _site/ travis@$TEST_SERVER:~/www/data/$BASEURL
+rsync -rvzh -e "ssh -i scripts/deploy/deploy_key_test -oStrictHostKeyChecking=no" --checksum --link-dest="../develop" _site/ travis@$TEST_SERVER:~/www/data/$BASEURL
 rm scripts/deploy/deploy_key*
