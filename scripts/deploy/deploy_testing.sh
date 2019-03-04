6eda217a859fd5ccd4f683e5ef0a2ca6e0f8458a
Douglas Ashton <douglas.j.ashton@gmail.com>
2019-03-04 15:54:55 +0000

    linkdest to develop

--- scripts/deploy/deploy_testing.sh
+++ scripts/deploy/deploy_testing.sh
@@ -23,8 +23,8 @@ rm scripts/deploy/keys.tar
 chmod 600 ./scripts/deploy/deploy_key_test
 eval `ssh-agent -s`
 ssh-add scripts/deploy/deploy_key_test
-#rm scripts/deploy/deploy_key*
 
 # Push the files over, removing anything existing already.
 ssh -oStrictHostKeyChecking=no travis@$TEST_SERVER "rm -rf ~/www/data/$BASEURL || true"
-rsync -rvz -e "ssh -i scripts/deploy/deploy_key_test -oStrictHostKeyChecking=no" --checksum --link-dest="../sdg-indicators" _site/ travis@$TEST_SERVER:~/www/data/$BASEURL
+rsync -rvz -e "ssh -i scripts/deploy/deploy_key_test -oStrictHostKeyChecking=no" --checksum --link-dest="../develop" _site/ travis@$TEST_SERVER:~/www/data/$BASEURL
+rm scripts/deploy/deploy_key*
