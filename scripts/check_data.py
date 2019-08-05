c438e29180633a39e1ba5f864affa71118df23d3
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2019-08-05 14:52:40 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -7,20 +7,6 @@ Created on 2017-10-04
 
 # %% setup
 
-import sdg
+import output
 
-def main():
-    """Run csv checks on all indicator csvs in the data directory"""
-    status = True
-    
-    status = status & sdg.check_all_csv()
-    status = status & sdg.check_all_meta()
-
-    return status
-
-if __name__ == '__main__':
-    status = main()
-    if(not status):
-        raise RuntimeError("Failed checks")
-    else:
-        print("Success")
+output.opensdg_output.validate()
