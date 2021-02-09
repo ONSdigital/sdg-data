caf7377e7344b461944beb37342f8a2f70ee68c9
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-02-09 15:22:07 +0000

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,20 +1,21 @@
 from sdg.open_sdg import open_sdg_build
 
-def alter_meta(meta):
-    if 'indicator_number' in meta:
-        indicator_id = meta['indicator_number']
-        id_parts = indicator_id.split('.')
-        target_id = id_parts[0] + '.' + id_parts[1]
-        goal_id = id_parts[0]
-    meta['goal_meta_link'] = 'https://unstats.un.org/sdgs/metadata/?Text=&Goal='+goal_id+'&Target='+target_id
-    tier_spreadsheet_url = 'https://unstats.un.org/sdgs/files/Tier%20Classification%20of%20SDG%20Indicators_28%20Dec%202020_web.xlsx'
+tier_spreadsheet_url = 'https://unstats.un.org/sdgs/files/Tier%20Classification%20of%20SDG%20Indicators_28%20Dec%202020_web.xlsx'
     tier_df = pd.read_excel(spreadsheet_url, "Updated Tier classification", usecols=[2,6], names=['indicator', 'tier'], header=1).dropna(axis=0, subset=["indicator"])
     tier_df=tier_df[tier_df["indicator"]!="\n"]
     for i in tier_df.index:
         indicator_code=tier_df.loc[i, "indicator"]
         tier_df.loc[i, "indicator"]=indicator_code.split(" ")[0]
     tier_df = tier_df.set_index(['indicator'])
-    meta['un_designated_tier']=tier_df.loc[indicator_id].name
+
+def alter_meta(meta):
+    if 'indicator_number' in meta:
+        indicator_id = meta['indicator_number']
+        id_parts = indicator_id.split('.')
+        target_id = id_parts[0] + '.' + id_parts[1]
+        goal_id = id_parts[0]
+        meta['goal_meta_link'] = 'https://unstats.un.org/sdgs/metadata/?Text=&Goal='+goal_id+'&Target='+target_id
+        meta['un_designated_tier']=tier_df.loc[indicator_id].name
     
     return meta
 
