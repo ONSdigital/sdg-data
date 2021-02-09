0adbbf24ba7fcd43f1548a429d0ab67a1d3c8a00
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-02-09 13:53:06 +0000

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,11 @@
 from sdg.open_sdg import open_sdg_check
 
+def alter_meta(meta):
+    meta['goal_meta_link'] = 'https://unstats.un.org/sdgs/metadata/?Text=&Goal='+meta['sdg_goal']+'&Target='+meta['target_id']
+    return meta
+
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta)
 
 # If everything was valid, perform the build.
 if not validation_successful:
