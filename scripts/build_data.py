c2c029ee6691410cdc025bc4f7fd50ecbcbacd46
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-02-09 13:53:37 +0000

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,7 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(meta):
+    meta['goal_meta_link'] = 'https://unstats.un.org/sdgs/metadata/?Text=&Goal='+meta['sdg_goal']+'&Target='+meta['target_id']
+    return meta
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
