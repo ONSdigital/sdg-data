141a07c2a4236a9b91e0bf2d146af18fe2ce7c30
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-01-28 11:15:21 +0000

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -44,7 +44,7 @@ def alter_meta(meta):
         
         if 'computation_units' in meta and meta['computation_units'] is not None:
             if 'Percentage (%)' in meta['computation_units']:
-                meta['graph_limits']=[{"unit:":"Percentage (%)", "minimum":0, "maximum":100}]
+                meta['graph_limits']=[{"unit":"Percentage (%)", "minimum":0, "maximum":100}]
 
         if 'standalone' not in meta:
             if tier_df is not None:
